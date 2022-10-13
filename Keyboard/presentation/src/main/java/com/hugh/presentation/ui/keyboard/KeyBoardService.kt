7d3150acb9849dde85a0112323d9c8181f39b285
DavidKwon7 <khjunejune10@gmail.com>
2022-10-13 18:02:35 +0900

    feat: manifest service 추가

---
+++ Keyboard/presentation/src/main/java/com/hugh/presentation/ui/keyboard/KeyBoardService.kt
@@ -0,0 +1,11 @@
+package com.hugh.presentation.ui.keyboard
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+
+class KeyBoardService : InputMethodService() {
+
+}
