4894f2b6611f5317f7bf9f2507324c0f9299a538
DavidKwon7 <khjunejune10@gmail.com>
2022-10-13 18:31:10 +0900

    feat: 한글 키보드 구현

--- Keyboard/presentation/src/main/java/com/hugh/presentation/ui/keyboard/KeyBoardService.kt
+++ Keyboard/presentation/src/main/java/com/hugh/presentation/ui/keyboard/KeyBoardService.kt
@@ -2,10 +2,66 @@ package com.hugh.presentation.ui.keyboard
 
 import android.inputmethodservice.InputMethodService
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import com.hugh.presentation.R
 
 class KeyBoardService : InputMethodService() {
 
+    lateinit var keyboardView: LinearLayout
+    lateinit var keyboardFrame: FrameLayout
+    lateinit var keyboardKorean: KeyboardKorean
+    //lateinit var keyboardEnglish:KeyboardEnglish
+
+
+    val keyboardInterationListener = object : KeyboardInterationListener {
+        override fun modechange(mode: Int) {
+            currentInputConnection.finishComposingText()
+            when(mode) {
+                /*0 -> {
+                    keyboardFrame.removeAllViews()
+                    keyboardEnglish.inputConnection = currentInputConnection
+                    keyboardFrame.addView(keyboardEnglish.getLayout())
+
+                }*/
+                1 -> {
+                    keyboardFrame.removeAllViews()
+                    keyboardKorean.inputConnection = currentInputConnection
+                    keyboardFrame.addView(keyboardKorean.getLayout())
+                }
+            }
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as LinearLayout
+        keyboardFrame = keyboardView.findViewById(R.id.keyboard_frame)
+    }
+
+    override fun onCreateInputView(): View {
+        keyboardKorean = KeyboardKorean(applicationContext, layoutInflater, keyboardInterationListener)
+        //keyboardEnglish = KeyboardEnglish(applicationContext, layoutInflater, keyboardInterationListener)
+        keyboardKorean.inputConnection = currentInputConnection
+        keyboardKorean.init()
+        //keyboardEnglish.inputConnection = currentInputConnection
+        //keyboardEnglish.init()
+
+        return keyboardView
+    }
+
+    override fun updateInputViewShown() {
+        super.updateInputViewShown()
+        currentInputConnection.finishComposingText()
+        keyboardFrame.removeAllViews()
+        keyboardInterationListener.modechange(1)
+        /*if(currentInputEditorInfo.inputType == EditorInfo.TYPE_CLASS_NUMBER){
+            keyboardFrame.removeAllViews()
+            //keyboardFrame.addView(KeyboardNumpad.newInstance(applicationContext, layoutInflater, currentInputConnection, keyboardInterationListener))
+        }
+        else{
+            keyboardInterationListener.modechange(1)
+        }*/
+    }
 }
