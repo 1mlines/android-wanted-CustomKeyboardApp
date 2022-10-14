5e1d5e1fa72fcdfa3961f4b9b8f2d1dfb4390c74
DavidKwon7 <khjunejune10@gmail.com>
2022-10-14 19:57:59 +0900

    feat: KeyBoardAction 생성

---
+++ Keyboard/presentation/src/main/java/com/hugh/presentation/action/keyboard/KeyBoardAction.kt
@@ -0,0 +1,9 @@
+package com.hugh.presentation.action.keyboard
+
+sealed class KeyBoardAction {
+
+    object Space: KeyBoardAction()
+    object Delete: KeyBoardAction()
+    object CAPS: KeyBoardAction()
+
+}
